222a8a3ff5d1ae304a1bf298b83096c52542ce99
fonglh <fonglh@gmail.com>
2016-09-15 10:34:45 +0800

    Try out various test types.
    Make it easy to control pass/fail for each test.

---
+++ prog_question_test/submission/template.py
@@ -0,0 +1,4 @@
+# return True if you want the test to pass
+# each test passes in the test type and a number
+def test_result(test_types, numbers):
+    return True
---
+++ prog_question_test/tests/prepend.py
@@ -0,0 +1,8 @@
+import unittest
+# Needs xmlrunner: pip install unittest-xml-reporting
+import xmlrunner
+import sys
+import timeout_decorator
+# begin prepend code
+
+# end prepend code
